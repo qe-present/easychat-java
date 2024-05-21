b19d08ae2376711f52ceae54135110ba5c2230a6
qe-present <2664481691@qq.com>
2024-05-21 13:01:17 +0800

    add

---
+++ src/main/java/com/easychat/EasyChatApplication.java
@@ -0,0 +1,15 @@
+package com.easychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 作者:疏狂难除
+ * 时间:2024-05-21
+ */
+@SpringBootApplication
+public class EasyChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyChatApplication.class, args);
+    }
+}
